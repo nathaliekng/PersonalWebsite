c1cddfa5350a67313c9aebf1069c5b91891760a9
Nathalie Ng <41026746+nathaliekng@users.noreply.github.com>
2024-05-03 21:49:24 -0400

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,23 +1,35 @@
-import logo from './logo.svg';
 import './App.css';
+import Sidenav from './components/Sidenav.js';
+import {BrowserRouter as Router, Route, Routes, Link } from "react-router-dom";
+import Home from "./pages/Home";
+import Contact from "./pages/Contact";
+import PageNotFound from "./pages/PageNotFound";
+import About from "./pages/About";
+import Projects from "./pages/Projects";
+import {useState} from 'react';
 
 function App() {
+  const [inactive, setInactive] = useState(false);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Sidenav 
+          onCollapse={(inactive) => {
+            console.log(inactive);
+            setInactive(inactive);}}
+        />
+        <div className={`container ${inactive ? "inactive" : ""}`}>
+          <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/contact" element={<Contact />} />
+              <Route path="/about" element={<About />} />
+              <Route path="/projects" element={<Projects />} />
+              <Route path="*" element={<PageNotFound/>}/>
+          </Routes>
+        </div>
+
+      </Router>
+        
     </div>
   );
 }
