3237b04e2700da902f05851e973c21bbfa707a34
Nathalie Ng <41026746+nathaliekng@users.noreply.github.com>
2024-05-10 16:40:00 -0400

    added to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -23,11 +23,40 @@ function Home() {
         />    
         </div>  
 
-        
       </div>
-
-      <div className = "page-body">
-
+      <div className="about-details-container">
+        <span>
+          <div className="about-details">
+            <div className="heading">
+              Education
+            </div>
+            <div className="details">
+              BSc. in Computer Science from Toronto Metropolitan University
+            </div>
+             <div>
+              <RedirectButton
+              name="Projects"
+              to="/projects"
+              className="page-redirect-container"
+              />    
+            </div>
+          </div>
+          <div className="about-details">
+            <div className="heading">
+              Stack
+            </div>
+            <div className="details">
+              I am most comfortable with Java, and have experience with other languages such as Python. I am currently working to improve my JavaScript skills.
+            </div>
+            <div>
+              <RedirectButton
+              name="Stack"
+              to="/stack"
+              className="page-redirect-container"
+              />    
+            </div>
+          </div>
+        </span>
       </div>
       
     </div>
