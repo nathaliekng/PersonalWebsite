7bc27da33a4a0d22d8164551ec636cc0bbff8a51
Nathalie Ng <41026746+nathaliekng@users.noreply.github.com>
2024-05-20 21:58:58 -0400

    debugged netlify errors

--- src/components/DownloadButton.js
+++ src/components/DownloadButton.js
@@ -1,10 +1,7 @@
-import {NavLink} from "react-router-dom";
-
-
 const DownloadButton = (props) => {
     const {name, file, className, downloadName} = props;
   return (
-    <a href={file} target="_blank" className={className} download={downloadName}>{name}</a>
+    <a href={file} className={className} download={downloadName}>{name}</a>
   )
 }
 
